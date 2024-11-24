4ebcb3763724ccd81009a8582568714618852598
Liudmila <leroshkina@mail.ru>
2024-11-24 08:30:12 +0300

    модуль 27

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,4 @@
-from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from catalog.forms import StyleFormsMixin
 from users.models import User
 
